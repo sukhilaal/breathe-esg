d4304f86759e3afc1c76c30eb5d0160a6bf86188
Om Aman Srivastava <osrivastava50_be23@thapar.edu>
2026-05-27 10:14:04 +0530

    Serve health response at API root URL

--- backend/urls.py
+++ backend/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from core.views import health
+
 urlpatterns = [
+    path("", health),
     path('admin/', admin.site.urls),
     path("api/", include("core.urls")),
 ]
